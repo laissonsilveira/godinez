c5b4f7b1d03adb1270eb07cfb0fc6510cb6f317a
Emerson Mendes <emerson.emendes@gmail.com>
2017-07-26 02:13:02 -0300

    iniciando vue.js

--- app/static/src/index.js
+++ app/static/src/index.js
@@ -1,8 +1,23 @@
-import Teste from "./teste";
+import VueRouter from '../lib/vue-router/dist/vue-router';
+import App from './components/App.vue';
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentA.vue';
 
-new Vue({
-    el: '#app',
-    data: {
-        message: new Teste().getName()
+Vue.use(VueRouter)  
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'component-a',
+      component: ComponentA
     }
+  ]
+});
+
+new Vue({  
+  el: 'body',
+  components: { 
+  	App 
+  }
 });
